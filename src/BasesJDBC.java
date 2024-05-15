368ee09d4c0f9564656039e44ade2fa04270aea3
rloup <rloup@172.16.137.184>
2024-05-15 17:51:13 +0200

    J'ai presque fini la base de donnée JDBC, il faudra modifier les create Table (clef primaire, clef etrangere

--- src/BasesJDBC.java
+++ src/BasesJDBC.java
@@ -1,17 +1,20 @@
-import java.beans.Statement;
+import java.sql.Statement;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class BasesJDBC {
-
+	
+//----------------------------------------------------------------------------------------------------------------------//
 	public static void DELETE(String[] args) throws Exception {
 		Class.forName( "org.hsqldb.jdbcDriver" );
 		String url = "jdbc:hsqldb:file:database"+File.separator+"basic;shutdown=true";
 		String login = "sa";
 		String password = "";
 		try (Connection connection = DriverManager.getConnection( url, login, password )){
-			String requete = "DROP TABLE ROUTES IF EXISTS;"
+			String requete="";
+			
+			requete = "DROP TABLE ROUTES IF EXISTS;"
 					+ "DROP TABLE CLIENTS IF EXISTS;"
 					+ "DROP TABLE ENTREPOTS IF EXISTS;"
 					+ "DROP TABLE SITES IF EXISTS;";
@@ -19,24 +22,55 @@ public class BasesJDBC {
 			try ( Statement statement = connection.createStatement() ) {
 				statement.executeUpdate( requete );
 			}
+		}
+	}
 
+//----------------------------------------------------------------------------------------------------------------------//
+	public static void CREATE(String[] args) throws Exception {
+		Class.forName( "org.hsqldb.jdbcDriver" );
+		String url = "jdbc:hsqldb:file:database"+File.separator+"basic;shutdown=true";
+		String login = "sa";
+		String password = "";
+		try (Connection connection = DriverManager.getConnection( url, login, password )){	
+			String requete ="";
 			requete = "CREATE TABLE ROUTES ("
 					+"origine int,"
 					+"destination int,"
-					+"PRIMARY KEY(orignine))";
-			try ( Statement statement = connection.createStatement() ) {
-				statement.executeUpdate( requete );
-			}
+					+"PRIMARY KEY(orignine))"
 
-			requete = "INSERT INTO citation (annee, citation, auteur) VALUES"
-					+"(1947, 'La simplicite est la reussite absolue', 'Frederic Chopin'),"
-					+"(1979, 'Le bonheur, tu sauras que c''est la simplicite', 'Jacques Brillant'),"
-					+"(1986, 'La simplicite est la cle de la reussite','Andre Rochette')";
+			+"CREATE TABLE CLIENTS ("
+			+"nom varchar(30),"
+			+"prenom varchar(30),"
+			+"Id_site int"
+			+"PRIMARY KEY(Id_site))"
+
+			+"CREATE TABLE ENTREPROTS ("
+			+"Id_entreprot int,"
+			+"Id_site int,"
+			+"cout_fixe int,"
+			+"stock int,"
+			+"PRIMARY KEY(Id_entreprot),"
+			+"FOREIGN KEY(Id_site))"
+
+			+"CREATE TABLE SITES ("
+			+"Id_site int,"
+			+"x int,"
+			+"y int,"
+			+"PRIMARY KEY(Id_site))"
+			+"FOREIGN KEY(Id_site))";
+
 			try ( Statement statement = connection.createStatement() ) {
 				statement.executeUpdate( requete );
 			}
 		}
 	}
 
+//--------------------------------------------------------------------------------------------------------------------//
+	public static void main(String[] args) throws Exception {
+		BasesJDBC CREATE = new BasesJDBC();
+		CREATE.DELETE();
+		CREATE.CREATE();
+		System.out.println("CREATION DE LA BASE DE DONNéEs");
+	}
 }
 
