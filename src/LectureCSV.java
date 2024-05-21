67ae94a9b41ce513f30bdc64ddcc2e457c941e23
rloup <rloup@172.16.137.184>
2024-05-21 17:58:11 +0200

    Avancement class LectureCSV

--- src/LectureCSV.java
+++ src/LectureCSV.java
@@ -1,4 +1,18 @@
+import java.util.LinkedList;
 
 public class LectureCSV {
-
+	
+	private LinkedList<Route> Routes;
+	private LinkedList<Site> Sites;
+	private LinkedList<Entrepot> Entrepots;
+	private LinkedList<Client> Clients;
+	
+	
+	public LectureCSV(LinkedList<Route> routes, LinkedList<Site> sites, LinkedList<Entrepot> entrepots,
+			LinkedList<Client> clients) {
+		Routes = routes;
+		Sites = sites;
+		Entrepots = entrepots;
+		Clients = clients;
+	}
 }
