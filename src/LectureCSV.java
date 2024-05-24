c14ca40268a44803d83c553901bd2ad121c7490e
rloup <rloup@172.16.137.184>
2024-05-24 08:33:11 +0200

    creation class

--- src/LectureCSV.java
+++ src/LectureCSV.java
@@ -1,18 +1,18 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class LectureCSV {
 	
-	private LinkedList<Route> Routes;
-	private LinkedList<Site> Sites;
-	private LinkedList<Entrepot> Entrepots;
-	private LinkedList<Client> Clients;
+	private ArrayList<Route> Routes;
+	private ArrayList<Entrepot> Entrepots;
+	private ArrayList<Client> Clients;
 	
 	
-	public LectureCSV(LinkedList<Route> routes, LinkedList<Site> sites, LinkedList<Entrepot> entrepots,
-			LinkedList<Client> clients) {
-		Routes = routes;
-		Sites = sites;
-		Entrepots = entrepots;
-		Clients = clients;
+	public LectureCSV() throws Exception{
+		this.Routes = new ArrayList<Route>();
+		this.Entrepots = new ArrayList<Entrepot>();
+		this.Clients = new ArrayList<Client>();
 	}
+	
+	
 }
