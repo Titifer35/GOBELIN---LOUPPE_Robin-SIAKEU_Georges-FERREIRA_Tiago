1286329dfc778c79fa5a0f518d72b4f476d865e2
LENOVO T450 i5 <LENOVO T450 i5@172.16.134.233>
2024-05-27 16:09:11 +0100

    Algo de Djiskra

--- src/Plus_court_chem.java
+++ src/Plus_court_chem.java
@@ -3,6 +3,10 @@ import java.util.List;
 public class Plus_court_chem {
 
 	public int[][] plus_court_chemin(List<Integer> sites) {
+		int n = sites.size();
+        int[] dist = new int[n];
+        boolean[] visited = new boolean[n];
+        
 		return null;
 	}
 
