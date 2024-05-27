e4c575b4ebdc4284489e219c92e4bf7913212573
LENOVO T450 i5 <LENOVO T450 i5@172.16.134.233>
2024-05-27 16:17:09 +0100

    Algo de Djiskra

--- src/Plus_court_chem.java
+++ src/Plus_court_chem.java
@@ -1,12 +1,37 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Plus_court_chem {
+	
+	static class Edge {
+        int target;
+        int weight;
+
+        public Edge(int target, int weight) {
+            this.target = target;
+            this.weight = weight;
+        }
+    }
 
 	public int[][] plus_court_chemin(List<Integer> sites) {
 		int n = sites.size();
-        int[] dist = new int[n];
-        boolean[] visited = new boolean[n];
+		List<List<Edge>> sites = new ArrayList<>();
         
+		for (int i = 0; i < n; i++) {
+			sites.add(new ArrayList<>());
+        }
+        
+		int[][] distances = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            distances[i] = dijkstra(sites, i);
+        }
+
+        return distances;
+	}
+
+	private int[] dijkstra(List<List<Plus_court_chem.Edge>> sites, int i) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
