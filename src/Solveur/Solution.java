7b527e73ed40b11a0ccfe358d7147d38d1f97b67
dapai <dapai@172.16.134.160>
2024-06-07 16:10:12 +0200

    commentaire

--- src/Solveur/Solution.java
+++ src/Solveur/Solution.java
@@ -1,8 +1,12 @@
-package Solveur;
+	package Solveur;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
+//---------------------------------------------------------------------------------------------------
+//	
+//---------------------------------------------------------------------------------------------------
 
 public class Solution {
 	public static void main(String[] args) throws IOException {
